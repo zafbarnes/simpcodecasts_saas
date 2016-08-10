730159cc4c10766ad26b5a838828af07274ff2da
zafbarnes <zaf.barnes@gmail.com>
2016-08-10 10:56:16 +0000

    Modifide devise for stripe

---
+++ db/migrate/20160810092905_add_stripe_customer_token_to_users.rb
@@ -0,0 +1,4 @@
+class AddStripeCustomerTokenToUsers < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808200012) do
+ActiveRecord::Schema.define(version: 20160810092905) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
