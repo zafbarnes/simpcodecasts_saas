eb04bf94993f8991a803211d1d1cba16f7c19af8
zafbarnes <zaf.barnes@gmail.com>
2016-08-15 15:45:47 +0000

    Ammended controller, users and added users show action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,7 +10,7 @@ class ProfilesController < ApplicationController
         @profile = @user.build_profile(profile_params) 
         if @profile.save
             flash[:success] = "Profile Updated!"
-            redirect_to_user_path(params[:user_id])
+            redirect_to user_path(params[:user_id] )
         else
             render action: :new
         end    
